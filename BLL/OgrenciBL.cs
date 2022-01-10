2328a6acc62e53a73b042dffc063900c88967924
LAB-PC <LAB-PC@DESKTOP-TEBFMCJ>
2022-01-10 11:16:40 +0300

    10012022_1116

--- BLL/OgrenciBL.cs
+++ BLL/OgrenciBL.cs
@@ -11,8 +11,7 @@ namespace BLL
     public class OgrenciBL
     {
         public bool OgrenciEkle(Ogrenci ogr)
-        {
-            ogr = null;
+        {            
             if (ogr==null)
             {
                 throw new NullReferenceException("Ogrenci Eklerken Referans Null Geldi");
@@ -22,6 +21,25 @@ namespace BLL
             {
                 Helper hlp = new Helper();
                 int sonuc = hlp.ExecuteNonQuery($"Insert into tblOgrenciler(Ad,Soyad,Numara,TcKimlik,SinifId)values('{ogr.Ad}','{ogr.Soyad}','{ogr.Numara}','{ogr.Tckimlik}',{ogr.Sinifid})");
+                return sonuc > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool OgrenciGuncelle(Ogrenci ogr)
+        {
+            if (ogr == null)
+            {
+                throw new NullReferenceException("Ogrenci Güncellerken Referans Null Geldi");
+            }
+
+            try
+            {
+                Helper hlp = new Helper();
+                int sonuc = hlp.ExecuteNonQuery($"Update tblOgrenciler set Ad='{ogr.Ad}',Soyad='{ogr.Soyad}',Numara='{ogr.Numara}',Sinifid={ogr.Sinifid} where OgrenciId={ogr.Ogrenciid}");
                 return sonuc > 0;
             }
             catch (Exception)
